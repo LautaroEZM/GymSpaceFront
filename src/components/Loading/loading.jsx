9476f8f3897ff9ad45a2e53d56a80c3620ebf599
Alexis Porcelano <alexisporcelano@gmail.com>
2023-11-30 01:06:27 -0300

    userServices muestra los servicios relacionados con el usuario

--- src/components/Loading/loading.jsx
+++ src/components/Loading/loading.jsx
@@ -1,18 +1,24 @@
-import Container from "@mui/material/Container";
+import { Box } from "@mui/material/";
 import { useEffect, useState } from "react";
 
 export default function Loading({ loading }) {
-
   const image =
     "https://firebasestorage.googleapis.com/v0/b/gymspace-d93d8.appspot.com/o/loading.gif?alt=media&token=9b285b61-c22f-4f7f-a3ca-154db8d99d73";
 
   return (
-    <Container maxWidth="xs">
+    <Box
+      sx={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        padding: 6,
+      }}
+    >
       {loading ? (
         <>
           <img src={image} />
         </>
       ) : null}
-    </Container>
+    </Box>
   );
 }
