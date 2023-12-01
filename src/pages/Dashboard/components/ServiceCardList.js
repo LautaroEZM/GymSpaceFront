de421d38ba2947670f725b82711ad558bf5edec7
LautaroEM <lautaro.mongelo@gmail.com>
2023-11-30 22:05:14 -0300

    cambios en el dashboard

--- src/pages/Dashboard/components/ServiceCardList.js
+++ src/pages/Dashboard/components/ServiceCardList.js
@@ -1,23 +1,31 @@
-import React from 'react';
-import ServiceCard from './ServiceCard';
-import { Box } from '@mui/system';
+import React from "react";
+import ServiceCard from "./ServiceCard";
+import { Box } from "@mui/system";
 
 const ServiceCardList = ({ services }) => {
-
-    return (
-        <Box>
-            {services.map((service, index) => (
-                <Box key={index} style={{ marginBottom: '20px' }}>
-                    <ServiceCard
-                        serviceName={service.serviceName}
-                        extraInfo={service.extraInfo}
-                        enrolledPeople={service.enrolledPeople}
-                        coaches={service.coaches}
-                    />
-                </Box>
-            ))}
+  return (
+    <Box>
+      <Box
+        sx={{
+          color: "#f3a143",
+          fontSize: "40px",
+          textAlign: "center",
+        }}
+      >
+        LIST OF SERVICES
+      </Box>
+      {services.map((service, index) => (
+        <Box key={index} style={{ marginBottom: "20px" }}>
+          <ServiceCard
+            serviceName={service.serviceName}
+            extraInfo={service.extraInfo}
+            enrolledPeople={service.enrolledPeople}
+            coaches={service.coaches}
+          />
         </Box>
-    );
+      ))}
+    </Box>
+  );
 };
 
 export default ServiceCardList;
